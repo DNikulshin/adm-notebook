689329ee460733a6bac30b9f27c9b98af5bc47aa
Dmitriy Nikulshin <98643483+DNikulshin@users.noreply.github.com>
2024-05-03 22:25:44 +0000

    edit config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
-  base: "/adm-notebook/",
   plugins: [
     vue(),
     VitePWA({
