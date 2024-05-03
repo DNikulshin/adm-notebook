78ce1c009ef833ff08d49a66db915efbf1af23da
Dmitriy Nikulshin <98643483+DNikulshin@users.noreply.github.com>
2024-05-03 20:00:14 +0000

    add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
+  base: "/adm-notebook/",
   plugins: [
     vue(),
-    VitePWA({ 
-      registerType: 'autoUpdate',
-     
+    VitePWA({
+      registerType: "autoUpdate",
+
       workbox: {
         cleanupOutdatedCaches: true,
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff,woff2,webmanifest}'],
+        globPatterns: [
+          "**/*.{js,css,html,ico,png,svg,json,vue,txt,woff,woff2,webmanifest}",
+        ],
       },
-      injectRegister: "auto"
-     })
-  ]
-})
+      injectRegister: "auto",
+    }),
+  ],
+});
